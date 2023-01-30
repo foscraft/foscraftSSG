45e4e6e11ec0d088cd9f3c781a8936b1fefb5056
foscraft <anyaribari@gmail.com>
2023-01-30 15:56:26 +0300

    ch-itearate-over-files

--- ssg.py
+++ ssg.py
@@ -1,25 +1,35 @@
+import os
+from datetime import datetime
+
 from jinja2 import (
     Environment,
     PackageLoader,
 )
 from markdown2 import markdown
 
-with open("./components/turkish-pide.md", "r") as f:
-    parsed_md = markdown(f.read(), extras=["metadata"])
+articles = {}
 
-    env = Environment(
-        loader=PackageLoader(
-            "ssg",
-            "./templates",
-        )
-    )
-    test_template = env.get_template("test.html")
+for md_article in os.listdir("components"):
+    file_path = os.path.join("components", md_article)
 
+    with open(file_path, "r") as f:
+        articles[md_article] = markdown(f.read(), extras=["metadata"])
 
-data = {
-    "content": parsed_md,
-    "title": parsed_md.metadata["title"],
-    "date": parsed_md.metadata["date"],
-}
+        articles = {
+            article: articles[article]
+            for article in sorted(
+                articles,
+                key=lambda article: datetime.strptime(
+                    articles[article].metadata["date"], "%Y-%m-%d"
+                ),
+                reverse=True,
+            )
+        }
+        env = Environment(loader=PackageLoader("ssg", "templates"))
+        home_template = env.get_template("home.html")
+        article_template = env.get_template("article.html")
 
-print(test_template.render(post=data))
+        articles_metadata = [
+            articles[article].metadata for article in articles
+        ]
+        home_html = home_template.render(articles=articles_metadata)
